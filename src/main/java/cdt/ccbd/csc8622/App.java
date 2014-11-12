03aa4f8fdf47a15afdeac1225aad7fe0c91b42cf
saleh <e106625@hotmail.com>
2014-11-12 20:18:42 +0000

    initial commit

---
+++ src/main/java/cdt/ccbd/csc8622/App.java
@@ -0,0 +1,13 @@
+package cdt.ccbd.csc8622;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
